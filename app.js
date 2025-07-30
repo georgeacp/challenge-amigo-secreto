38492e2a2371954cde4548eb46bc6931026f5f8f
georgeacp <133732710+georgeacp@users.noreply.github.com>
2025-07-30 18:26:30 -0500

    feat: Enhance friend addition logic and update UI structure

--- app.js
+++ app.js
@@ -3,13 +3,44 @@
 let amigos = [];
 
 function agregarAmigo() {
-  let amigo = document.getElementById("amigo").value;
-  if (amigo == "") {
+  // Obtener el valor del input y eliminamos espacios en blanco
+  let amigo = document.getElementById("amigo").value.trim();
+
+  // Validar que el campo no este vacio
+  if (amigo === "") {
     alert("Por favor, inserte un nombre.");
-  } else {
-    amigos.push(amigo);
-    document.getElementById("amigo").value = "";
+    return;
   }
 
-  console.log(amigos);
+  // Validar que el nombre no exista en la lista
+  if (amigos.includes(amigo)) {
+    alert("Esta amigo ya esta en la lista");
+    document.getElementById("amigo").value = "";
+    return;
+  }
+
+  // Agregar el nombre al array
+  amigos.push(amigo);
+
+  // Limpiar el input
+  document.getElementById("amigo").value = "";
+
+  // Actualizar la lista
+  mostrarAmigos();
 }
+
+function mostrarAmigos() {
+  const listaAmigos = document.getElementById("listaAmigos");
+
+  // Limpiar la lista antes de volver a mostrar
+  listaAmigos.innerHTML = "";
+
+  // Iterar sobre el arreglo
+  for (let i = 0; i < amigos.length; i++) {
+    const li = document.createElement("li");
+    li.textContent = amigos[i];
+    listaAmigos.appendChild(li);
+  }
+}
+
+console.log(amigos);
